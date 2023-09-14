cb3ea7fa8af2411926a207792cfcb1e316caa6ef
Fozkun <s3924683@rmit.edu.vn>
2023-09-14 14:16:29 +0700

    Created a menu to contain many Guis and run

---
+++ src/packages/guis/builders/Menu.java
@@ -0,0 +1,95 @@
+package packages.guis.builders;
+
+import packages.ErrorHandlers;
+import java.util.HashMap;
+import java.util.Scanner;
+
+public class Menu{
+    private String id;
+    private String name;
+    private HashMap<String,Gui> guis;
+
+    private boolean keepRunning = true;
+    private String oldPointer = "1";
+    private String pointer = "1";
+
+    public Menu(String id, String name, HashMap<String,Gui> guis) {
+        this.id = id;
+        this.name = name;
+        this.guis = guis;
+    }
+
+    public Menu(String id, String name) {
+        this.id = id;
+        this.name = name;
+
+    }
+    public Gui run(){
+        if(!keepRunning){
+            keepRunning = true;
+        }
+
+        Scanner input = new Scanner(System.in);
+        int count = 0;
+
+        while (keepRunning){
+            Gui gui = guis.get(pointer);
+
+            String text = gui.getText();
+
+            boolean noMoreGuis = false;
+
+            if(count < 2){
+                System.out.println(text);
+            }else{
+                count = 0;
+            }
+
+            System.out.print("Input: ");
+
+            String inputResult = input.next();
+
+            for (String key : guis.keySet()) {
+                if(!key.equals(pointer)){
+                    if (key.contains(pointer) && key.length() > pointer.length()) {
+                    } else{
+                        noMoreGuis = true;
+                    }
+                }
+            }
+
+            if(noMoreGuis){
+                String guiOption = gui.getOptions().get(inputResult);
+
+                if(guiOption == null){
+                    ErrorHandlers.noOptionsFromInput(inputResult);
+                    count++;
+                }else{
+                    if(guiOption.equals("Go back")){
+                        pointer = oldPointer;
+                    }else if(guiOption.equals("Exit")){
+                        keepRunning = false;
+                    }else{
+                        return gui;
+                    }
+                };
+            }else{
+                String index = pointer + " " + inputResult;
+
+                if(guis.containsKey(index)){
+                    oldPointer = pointer;
+                    pointer = index;}
+                else{
+                    count++;
+                    ErrorHandlers.noOptionsFromInput(inputResult);
+                }
+            }
+        }
+
+        return null;
+    }
+
+    public void stop(){
+        this.keepRunning = false;
+    }
+}
