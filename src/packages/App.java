95c4ae6c244c9c9a4e47848b73f3fa7953490ae1
Fozkun <s3924683@rmit.edu.vn>
2023-09-14 19:30:13 +0700

    Updated app class to handle user login data

--- src/packages/App.java
+++ src/packages/App.java
@@ -1,19 +1,39 @@
 package packages;
 
+import packages.guis.ControlPanel;
 import packages.guis.Login;
+import packages.users.PortManager;
+import packages.users.SystemAdmin;
 import packages.users.abstracts.User;
 import java.util.HashMap;
 
 public class App {
     private static HashMap<String, HashMap<String, User[]>>  users;
-    public App() {}
+    public App() {
+        users = new HashMap<>();
+    }
 
     public void start(){
-        System.out.println("System is starting!");
-
         try {
+            System.out.println("System is starting!");
+
             Login login = new Login();
-            login.run();
+            HashMap<String, String> userData = login.run();
+
+            String username = userData.get("username");
+            String password = userData.get("password");
+            String type = userData.get("type");
+
+            User user;
+            if(type.equals("systemAdmin")){
+                user = new SystemAdmin(username,password);
+            }else{
+                user = new PortManager(username,password);
+            }
+
+            ControlPanel controlPanel = new ControlPanel(user);
+
+            controlPanel.run();
         } catch (Exception e) {
             throw new RuntimeException(e);
         }
